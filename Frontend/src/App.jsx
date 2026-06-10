49549984456d16236fff11581742cb6ab93ad0cb
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-06-09 23:41:36 -0700

    Categories

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Root from "./Utils/Root.jsx";
 import Login from "./Pages/Login.jsx";
 import ProtectedRoutes from "./Utils/ProtectedRoutes.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
+import Categories from "./Components/Categories.jsx";
 
 function App() {
 
@@ -13,6 +14,13 @@ function App() {
         <Route path="/" element={<Root />} />
         <Route path="/admin-dashboard" element={<ProtectedRoutes requiredRole={['admin']}><Dashboard/></ProtectedRoutes>}>
           <Route index element={<h1>Summary of Admin Dashboard</h1>} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="products" element={<h1>Products</h1>} />
+          <Route path="suppliers" element={<h1>Suppliers</h1>} />
+          <Route path="orders" element={<h1>Orders</h1>} />
+          <Route path="users" element={<h1>Users</h1>} />
+          <Route path="profile" element={<h1>Profile</h1>} />
+          <Route path="logout" element={<h1>Logout</h1>} />
         </Route>
         <Route path="/customer/dashboard" element={<h1>Customer Dashboard</h1>} />
         <Route path="/login" element={<Login />} />
--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { FaBox, FaCog, FaHome, FaLocationArrow, FaShoppingCart, FaSignOutAlt, FaTable, FaTruck, FaUsers } from 'react-icons/fa';
+import { FaBox, FaCog, FaHome, FaShoppingCart, FaSignOutAlt, FaTable, FaTruck, FaUsers } from 'react-icons/fa';
 import { NavLink } from 'react-router';
 
 const Sidebar = () => {
   const menuItems = [
-    { name: 'Dashboard', path: '/admin-dashboard', icon:<FaHome /> },
-    { name: 'Categories', path: '/admin-dashboard/categories', icon:<FaTable /> },
-    { name: 'Products', path: '/admin-dashboard/products', icon:<FaBox /> },
-    { name: 'Suppliers', path: '/admin-dashboard/suppliers', icon:<FaTruck /> },
-    { name: 'Orders', path: '/admin-dashboard/orders', icon:<FaShoppingCart /> },
-    { name: 'Users', path: '/admin-dashboard/users', icon:<FaUsers /> },
-    { name: 'Profile', path: '/admin-dashboard/profile', icon:<FaCog /> },
-    { name: 'Logout', path: '/admin-dashboard/logout', icon:<FaSignOutAlt /> },
+    { name: 'Dashboard', path: '/admin-dashboard', icon:<FaHome />, isParent: true },
+    { name: 'Categories', path: '/admin-dashboard/categories', icon:<FaTable />, isParent: false },
+    { name: 'Products', path: '/admin-dashboard/products', icon:<FaBox />, isParent: false },
+    { name: 'Suppliers', path: '/admin-dashboard/suppliers', icon:<FaTruck />, isParent: false },
+    { name: 'Orders', path: '/admin-dashboard/orders', icon:<FaShoppingCart />, isParent: false },
+    { name: 'Users', path: '/admin-dashboard/users', icon:<FaUsers />, isParent: false },
+    { name: 'Profile', path: '/admin-dashboard/profile', icon:<FaCog />, isParent: false },
+    { name: 'Logout', path: '/admin-dashboard/logout', icon:<FaSignOutAlt />, isParent: false },
   ];
   return (
     <div className='flex flex-col h-screen bg-black text-white w-16 md:w-64 fixed'>
       <div className="h-16 flex flex-items justify-center">
-        <span className='hidden md:block text-xl font-bold'>Seven Hills Inventory Store</span>
-        <span className='md:hidden text-xl font-bold'>SH Store</span>
+        <span className='hidden md:block text-xl font-bold'>Seven Hills Inventory</span>
+        <span className='md:hidden text-xl font-bold'>Store</span>
       </div>
       <div>
         <ul className="space-y-2 p-2">
           {menuItems.map((item) => {
-            <li key={item.name} className={({ isActive }) => ( isActive )}>
-              <NavLink >
-                <span>{item.icon}</span>
-                <span>{item.name}</span>
+            return (<li key={item.name}>
+              <NavLink end={item.isParent} className={({ isActive }) => ( isActive ? "bg-gray-700" : "") + " flex items-center p-2 rounded-md hover:bg-gray-700 transition duration-200"} to={item.path}>
+                <span className='text-xl'>{item.icon}</span>
+                <span className='ml-4 hidden md:block'>{item.name}</span>
               </NavLink>
-            </li>
+            </li>)
           })}
         </ul>
       </div>
